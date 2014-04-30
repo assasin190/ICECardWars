49ccaaa9a547ea1276d49c0a521d91344af58c7f
Hades Reflection <s-soul@msn.com>
2014-05-01 01:33:50 +0700

    battlefield can run

--- src/main/Battlefield.java
+++ src/main/Battlefield.java
@@ -93,14 +93,14 @@ public class Battlefield extends JFrame {
 	public static JTextArea notify;
 	private JTextArea desc_area;
 	public static void main(final String[] args) {
-		final String s = args[0];
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					CardData.saveAllCardsToLocal();
 					Battlefield frame = new Battlefield(new Inw("{\"cv_uid\":\"595\",\"fb_id\":\"100003770583869\",\"firstname_en\":\"Pasin\",\"lastname_en\":\"Boonsermsuwong\",\"full_lp\":\"40\",\"full_mp\":\"5\",\"max_deck_size\":\"20\"}")
-					//				, new Inw("{\"cv_uid\":\"584\",\"fb_id\":\"1035721781\",\"firstname_en\":\"Min\",\"lastname_en\":\"Uswachoke\",\"full_lp\":\"40\",\"full_mp\":\"5\",\"max_deck_size\":\"20\"}"));
-					, new Inw(args[0]));
+								, new Inw("{\"cv_uid\":\"584\",\"fb_id\":\"1035721781\",\"firstname_en\":\"Min\",\"lastname_en\":\"Uswachoke\",\"full_lp\":\"40\",\"full_mp\":\"5\",\"max_deck_size\":\"20\"}"));
+					//, new Inw();
 					//			frame.setVisible(true);
 					//			frame.run();
 
