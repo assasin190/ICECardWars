d59f934312bc10d0ba1f00a95f66e35fc94326d5
Hades Reflection <s-soul@msn.com>
2014-05-02 01:53:57 +0700

    main to static

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,6 +16,7 @@ import misc.Splash;
 import misc.SplashPanel;
 
 public class Main {
+	public static MainMenu main;
 	static Socket con;
 	static PrintWriter out;
 	static BufferedReader in;
@@ -28,7 +29,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		MainMenu main = new MainMenu(con);
+		main = new MainMenu(con);
 		Splash frame = new Splash();
 		frame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
 		
